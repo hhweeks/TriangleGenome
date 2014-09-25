a48fc2c5a541ef64ff36e1c285e20ec39ce7b695
psromero6 <promero6@unm.edu>
2014-09-25 00:42:55 -0600

    Statistics

---
+++ TriangleGenome/src/Statistics.java
@@ -0,0 +1,56 @@
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+
+public class Statistics {
+	private long fitScore;
+
+	public Statistics(BufferedImage genomeImage, BufferedImage masterImage) {
+		Raster masterRaster = masterImage.getRaster();
+		Raster genomeRaster = genomeImage.getRaster();
+
+		int redDistance;
+		int blueDistance;
+		int greenDistance;
+		long euclidianDistanceSqr=0;
+
+		int[] genomePixArray = new int[3];
+		int[] masterPixArray = new int[3];
+		int genomeRed;
+		for (int i = 0; i < Math.max(genomeImage.getWidth(),
+				masterImage.getWidth()); i++) {
+			for (int j = 0; j < Math.max(genomeImage.getHeight(),
+					masterImage.getHeight()); j++) {
+				redDistance = genomeRaster.getPixel(i, j, genomePixArray)[0]
+						- masterRaster.getPixel(i, j, masterPixArray)[0];
+				blueDistance = genomeRaster.getPixel(i, j, genomePixArray)[1]
+						- masterRaster.getPixel(i, j, masterPixArray)[1];
+				greenDistance = genomeRaster.getPixel(i, j, genomePixArray)[2]
+						- masterRaster.getPixel(i, j, masterPixArray)[2];
+
+				euclidianDistanceSqr += (redDistance * redDistance)
+						+ (blueDistance * blueDistance)
+						+ (greenDistance * greenDistance);
+
+			}
+			
+
+		}
+		fitScore=euclidianDistanceSqr;
+
+	}
+	
+	
+	public long getFitScore(){
+		
+		return fitScore;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
