53a71ec7839ce9fb511b178282f8a6f5760f086a
psromero6 <promero6@unm.edu>
2014-09-29 19:16:23 -0600

    Images added. ImagePanel.repaint() quick fix(still needs work)

--- TriangleGenome/src/Tribe.java
+++ TriangleGenome/src/Tribe.java
@@ -1,16 +1,19 @@
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 
 public class Tribe {
-	
+	Random rand=new Random();
 	ArrayList<Genome> genomeList= new ArrayList<>();
 	BufferedImage masterImage;
-	
+	public static final int STARTINGTRIBESIZE=200;
+	public static final int ENDINGTRIBESIZE=500;
 	public Tribe(BufferedImage image){
 		masterImage=image;
 		//populate genome list
-	for(int i=0;i<200;i++){
+	for(int i=0;i<STARTINGTRIBESIZE;i++){
 		genomeList.add(new Genome(masterImage));
 
 	}
@@ -25,6 +28,30 @@ public class Tribe {
 		genome.fitscore=Statistics.getFitScore(masterImage, GenomeUtilities.getBufferedImage(genome));
 		
 	}
+	}
+	
+	
+	public static void nextGeneration(){
+		//Collections.sort(genomeList);
+		//while(genomeList.size()<ENDINGTRIBESIZE){
+		Random rand =new Random();
+		for(int i=0;i<50;i++){
+		int index=(int) Math.abs(rand.nextGaussian()*66);
+
+		System.out.println(index);
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	public static void main(String[] args){
+		//()
+		nextGeneration();
 	}
 	
 
