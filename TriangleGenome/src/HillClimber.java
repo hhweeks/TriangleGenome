3cf0b9dd3ca027340c03e59f3bea76f8dd1d7715
psromero6 <promero6@unm.edu>
2014-10-07 16:53:33 -0600

    CrudeThreadingAdded

--- TriangleGenome/src/HillClimber.java
+++ TriangleGenome/src/HillClimber.java
@@ -3,172 +3,199 @@ import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.util.Random;
 
-public class HillClimber extends Thread {
-	// repeat supersedes revert
-	static Random rand = new Random();
-	public BufferedImage image;
-	boolean repeat;
-	boolean revert;
-	Gene lastGene;
-	// I need to to define input parameters based on which allele is which.
-	int lastAllele;
-	int lastShift;
+public class HillClimber extends Thread
+{
+  // repeat supersedes revert
+  static Random rand = new Random();
+  public BufferedImage image;
+  boolean repeat;
+  boolean revert;
+  Gene lastGene;
+  // I need to to define input parameters based on which allele is which.
+  int lastAllele;
+  int lastShift;
 
-	public HillClimber(BufferedImage img) {
-		image = img;
-		repeat = false;
-		revert = false;
+  public HillClimber(BufferedImage img)
+  {
+    image = img;
+    repeat = false;
+    revert = false;
+  }
 
-	}
+  public void climbLoop(Genome genome, int N)
+  {
+    for (int i = 0; i < N; i++)
+    {
+      repeat = climbStep(genome);
+    }
+  }
 
-	public void climbLoop(Genome genome, int N) {
+  public boolean climbStep(Genome myGenome)
+  {
+    long startScore = Statistics.getFitScore(GenomeUtilities.getBufferedImage(myGenome), image);
+    int maxBound;
 
-		for (int i = 0; i < N; i++) {
+    if (repeat)
+    {
+      maxBound = getAlleleBounds(myGenome, lastAllele);
+      if (lastAllele + lastShift < 0)
+      {
+        // make lastShift+lastAllele=0:
+        lastShift = -lastAllele;//
+        repeat = false;
+      }
+      if (lastAllele + lastShift > maxBound)
+      {
+        // make lastShift+lastAllele=maxBound
+        lastShift = maxBound - lastAllele;
+        repeat = false;
+      }
+      Mutate.exposeToRadiation(lastGene, lastAllele, lastShift);
+    } else if (revert)
+    {
+      Mutate.exposeToRadiation(lastGene, lastAllele, -lastShift);
+      revert = false;
+    } else
+    {
+      //mutate random gene at random allele
+      Gene mutateGene=getGene(myGenome);
+      int mutateAlleleIndex=getAllele(mutateGene);
+      maxBound = getAlleleBounds(myGenome, mutateAlleleIndex);
+      int shiftAmount = rand.nextInt(maxBound / 5);// shift by up to 20%
+      shiftAmount-=(maxBound / 10);// subtract, to leave shift by +/- 10
+      
+      int mutateAlleleValue=Mutate.getAlleleValue(mutateGene, mutateAlleleIndex);
+      if(mutateAlleleValue+shiftAmount>maxBound)mutateAlleleValue=maxBound;
+      if(mutateAlleleValue+shiftAmount<0)mutateAlleleValue=0;
+      
+      Mutate.exposeToRadiation(mutateGene, mutateAlleleIndex, shiftAmount);
 
-			repeat = climbStep(genome);
+      lastGene = mutateGene;
+      lastAllele = mutateAlleleIndex;
+      lastShift = shiftAmount;
+    }
 
-		}
+    long endScore = Statistics.getFitScore(
+        GenomeUtilities.getBufferedImage(myGenome), image);
+    if (endScore > startScore)
+      revert = true;
+    return endScore < startScore;
+  }
+  
+  public Gene getGene(Genome myGenome)
+  {
+    Gene myGene=myGenome.geneList.get(rand.nextInt(myGenome.NUM_GENES));
+    return myGene;
+  }
+  public int getAllele(Gene myGene)
+  {
+    int myAlleleIndex=rand.nextInt(myGene.NALLELE);// 10 alleles
+    return myAlleleIndex;
+  }
 
-	}
+  public int getAlleleBounds(Genome myGenome, int allele)
+  {
+    int maxBound = 0;
+    int colorMax = 255;
+    switch (allele)
+    {
+    case 0:
+      maxBound = myGenome.IMG_WIDTH;
+      break;
+    case 1:
+      maxBound = myGenome.IMG_HEIGHT;
+      break;
+    case 2:
+      maxBound = myGenome.IMG_WIDTH;
+      break;
+    case 3:
+      maxBound = myGenome.IMG_HEIGHT;
+      break;
+    case 4:
+      maxBound = myGenome.IMG_WIDTH;
+      break;
+    case 5:
+      maxBound = myGenome.IMG_HEIGHT;
+      break;
+    case 6:
+      maxBound = colorMax;
+      break;
+    case 7:
+      maxBound = colorMax;
+      break;
+    case 8:
+      maxBound = colorMax;
+      break;
+    case 9:
+      maxBound = colorMax;
+      break;
+    }
+    return maxBound;
+  }
 
-	public boolean climbStep(Genome genome) {
+  public Gene getWurstQuadrent(Genome genome)
+  {
+    long tmpscore = 0;
+    long score;
+    int tmp = 0;
+    for (int i = 0; i < 4; i++)
+    {
+      score = getLocalFit(genome.geneList.get(i));
+      if (score > tmpscore)
+      {
+        tmp = i;
+        tmpscore = score;
+      }
 
-		long startScore = Statistics.getFitScore(
-				GenomeUtilities.getBufferedImage(genome), image);
-		int maxBound;
+    }
 
-		if (repeat) {
-			maxBound = getAlleleBounds(genome, lastAllele);
-			if (lastAllele + lastShift < 0) {
-				// make lastShift+lastAllele=0:
-				lastShift = -lastAllele;//
-				repeat = false;
-			}
-			if (lastAllele + lastShift > maxBound) {
-				// make lastShift+lastAllele=maxBound
-				lastShift = maxBound - lastAllele;
-				repeat = false;
-			}
-			Mutate.exposeToRadiation(lastGene, lastAllele, lastShift);
-		} else if (revert) {
-			Mutate.exposeToRadiation(lastGene, lastAllele, -lastShift);
-			revert = false;
-		} else {
-			Gene mutateGene = genome.geneList.get(rand.nextInt(genome.NUM_GENES));
-			int allele = rand.nextInt(mutateGene.NALLELE);// 10 alleles
-			maxBound = getAlleleBounds(genome, allele);
-			int shiftAmount = rand.nextInt(maxBound / 5);// shift by up to 20%
-			shiftAmount -= (maxBound / 10);// subtract, to leave shift by +/- 10
+    return genome.geneList.get(tmp);
 
-			if (allele + shiftAmount < 0 || allele + shiftAmount > maxBound) {
-				shiftAmount *= (-1);
-			}
-			Mutate.exposeToRadiation(mutateGene, allele, shiftAmount);
+  }
 
-			lastGene = mutateGene;
-			lastAllele = allele;
-			lastShift = shiftAmount;
-		}
+  public long getLocalFit(Gene gene)
+  {
 
-		long endScore = Statistics.getFitScore(
-				GenomeUtilities.getBufferedImage(genome), image);
-		if(endScore > startScore) revert=true;
-		return endScore < startScore;
-	}
+    Raster raster = this.image.getRaster();
+    int red, blue, green;
+    long sum = 0;
 
-	public int getAlleleBounds(Genome genome, int allele) {
-		int maxBound = 0;
-		int colorMax = 255;
-		switch (allele) {
-		case 0:
-			maxBound = genome.IMG_WIDTH;
-			break;
-		case 1:
-			maxBound = genome.IMG_HEIGHT;
-			break;
-		case 2:
-			maxBound = genome.IMG_WIDTH;
-			break;
-		case 3:
-			maxBound = genome.IMG_HEIGHT;
-			break;
-		case 4:
-			maxBound = genome.IMG_WIDTH;
-			break;
-		case 5:
-			maxBound = genome.IMG_HEIGHT;
-			break;
-		case 6:
-			maxBound = colorMax;
-			break;
-		case 7:
-			maxBound = colorMax;
-			break;
-		case 8:
-			maxBound = colorMax;
-			break;
-		case 9:
-			maxBound = colorMax;
-			break;
-		}
-		return maxBound;
-	}
+    int minX = Math.min(gene.xpoints[0],
+        Math.min(gene.xpoints[1], gene.xpoints[2]));
+    int maxX = Math.max(gene.xpoints[0],
+        Math.max(gene.xpoints[1], gene.xpoints[2]));
+    int minY = Math.min(gene.ypoints[0],
+        Math.min(gene.ypoints[1], gene.ypoints[2]));
+    int maxY = Math.max(gene.ypoints[0],
+        Math.max(gene.ypoints[1], gene.ypoints[2]));
 
-	public Gene getWurstQuadrent(Genome genome) {
-		long tmpscore = 0;
-		long score;
-		int tmp = 0;
-		for (int i = 0; i < 4; i++) {
-			score = getLocalFit(genome.geneList.get(i));
-			if (score > tmpscore) {
-				tmp = i;
-				tmpscore = score;
-			}
+    for (int x = minX; x <= maxX; x++)
+    {
+      for (int y = minY; y <= maxY; y++)
+      {
+        if (gene.contains(x, y))
+        {
+          int[] pixel =
+          { 0, 0, 0 };
+          raster.getPixel(x, y, pixel);
 
-		}
+          red = (pixel[0] - gene.r);
+          red = red * red;
+          blue = pixel[0] - gene.b;
+          blue = blue * blue;
+          green = pixel[0] - gene.g;
+          green = green * green;
+          sum = red + green + blue;
 
-		return genome.geneList.get(tmp);
+          // System.out.println(red+";"+green+";"+blue);
 
-	}
+        }
 
-	public long getLocalFit(Gene gene) {
+      }
 
-		Raster raster = this.image.getRaster();
-		int red, blue, green;
-		long sum = 0;
+    }
 
-		int minX = Math.min(gene.xpoints[0],
-				Math.min(gene.xpoints[1], gene.xpoints[2]));
-		int maxX = Math.max(gene.xpoints[0],
-				Math.max(gene.xpoints[1], gene.xpoints[2]));
-		int minY = Math.min(gene.ypoints[0],
-				Math.min(gene.ypoints[1], gene.ypoints[2]));
-		int maxY = Math.max(gene.ypoints[0],
-				Math.max(gene.ypoints[1], gene.ypoints[2]));
-
-		for (int x = minX; x <= maxX; x++) {
-			for (int y = minY; y <= maxY; y++) {
-				if (gene.contains(x, y)) {
-					int[] pixel = { 0, 0, 0 };
-					raster.getPixel(x, y, pixel);
-
-					red = (pixel[0] - gene.r);
-					red = red * red;
-					blue = pixel[0] - gene.b;
-					blue = blue * blue;
-					green = pixel[0] - gene.g;
-					green = green * green;
-					sum = red + green + blue;
-
-					// System.out.println(red+";"+green+";"+blue);
-
-				}
-
-			}
-
-		}
-
-		return sum / ((maxX - minX) * (maxY - minY));
-	}
+    return sum / ((maxX - minX) * (maxY - minY));
+  }
 
 }
--- TriangleGenome/src/Mutate.java
+++ TriangleGenome/src/Mutate.java
@@ -1,139 +1,167 @@
 import java.util.Random;
 
-
 public class Mutate
 {
-  static Random rand=new Random();
-  static final int NUMALLELES=10;
-  static final int NUMCOORD=6;//number of alleles in a gene that are coord, rather than color values
+  static Random rand = new Random();
+  static final int NUMALLELES = 10;
+  static final int NUMCOORD = 6;// number of alleles in a gene that are coord,
+                                // rather than color values
   static int maxCoordShift;
   static Genome refGen;
-  
-  public static void exposeToRadiation(Genome myGenome)
-  {
-    refGen=myGenome;
-    Gene mutateGene=myGenome.geneList.get(rand.nextInt(myGenome.NUM_GENES));
-    int mutateWhat=rand.nextInt(NUMALLELES);
-    maxCoordShift=myGenome.IMG_WIDTH/5;    
-    
-    switch (mutateWhat)
-    {
-    //mutate coordinates
-    case 0:
-      mutateGene.xpoints[0]=mutateXCoord(mutateGene.xpoints[0]);
-      break;
-    case 1:
-      mutateGene.ypoints[0]=mutateYCoord(mutateGene.ypoints[0]);
-      break;
-    case 2:
-      mutateGene.xpoints[1]=mutateXCoord(mutateGene.xpoints[1]);
-      break;
-    case 3:
-      mutateGene.ypoints[1]=mutateYCoord(mutateGene.ypoints[1]);
-      break;
-    case 4:
-      mutateGene.xpoints[2]=mutateXCoord(mutateGene.xpoints[2]);
-      break;
-    case 5:
-      mutateGene.ypoints[2]=mutateYCoord(mutateGene.ypoints[2]);
-      break;
-    //mutate colors
-    case 6:
-      mutateGene.r=mutateColor(mutateGene.r);
-      break;
-    case 7:
-      mutateGene.g=mutateColor(mutateGene.g);
-      break;
-    case 8:
-      mutateGene.b=mutateColor(mutateGene.b);
-      break;
-    case 9:
-      mutateGene.a=mutateColor(mutateGene.a);
-      break;
-   //translate x/y coordinates
-    }
-  }
-  
+
+//  public static void exposeToRadiation(Genome myGenome)
+//  {
+//    refGen = myGenome;
+//    Gene mutateGene = myGenome.geneList.get(rand.nextInt(myGenome.NUM_GENES));
+//    int mutateWhat = rand.nextInt(NUMALLELES);
+//    maxCoordShift = myGenome.IMG_WIDTH / 5;
+//
+//    switch (mutateWhat)
+//    {
+//    // mutate coordinates
+//    case 0:
+//      mutateGene.xpoints[0] = mutateXCoord(mutateGene.xpoints[0]);
+//      break;
+//    case 1:
+//      mutateGene.ypoints[0] = mutateYCoord(mutateGene.ypoints[0]);
+//      break;
+//    case 2:
+//      mutateGene.xpoints[1] = mutateXCoord(mutateGene.xpoints[1]);
+//      break;
+//    case 3:
+//      mutateGene.ypoints[1] = mutateYCoord(mutateGene.ypoints[1]);
+//      break;
+//    case 4:
+//      mutateGene.xpoints[2] = mutateXCoord(mutateGene.xpoints[2]);
+//      break;
+//    case 5:
+//      mutateGene.ypoints[2] = mutateYCoord(mutateGene.ypoints[2]);
+//      break;
+//    // mutate colors
+//    case 6:
+//      mutateGene.r = mutateColor(mutateGene.r);
+//      break;
+//    case 7:
+//      mutateGene.g = mutateColor(mutateGene.g);
+//      break;
+//    case 8:
+//      mutateGene.b = mutateColor(mutateGene.b);
+//      break;
+//    case 9:
+//      mutateGene.a = mutateColor(mutateGene.a);
+//      break;
+//    // translate x/y coordinates
+//    }
+//  }
+
   public static void exposeToRadiation(Gene mutateGene, int allele, int shiftAmount)
   {
     switch (allele)
     {
-    //mutate coordinates
+    // mutate coordinates
     case 0:
-      mutateGene.xpoints[0]+=shiftAmount;
+      mutateGene.xpoints[0] += shiftAmount;
       break;
     case 1:
-      mutateGene.ypoints[0]+=shiftAmount;
+      mutateGene.ypoints[0] += shiftAmount;
       break;
     case 2:
-      mutateGene.xpoints[1]+=shiftAmount;
+      mutateGene.xpoints[1] += shiftAmount;
       break;
     case 3:
-      mutateGene.ypoints[1]+=shiftAmount;
+      mutateGene.ypoints[1] += shiftAmount;
       break;
     case 4:
-      mutateGene.xpoints[2]+=shiftAmount;
+      mutateGene.xpoints[2] += shiftAmount;
       break;
     case 5:
-      mutateGene.ypoints[2]+=shiftAmount;
+      mutateGene.ypoints[2] += shiftAmount;
       break;
-    //mutate colors
+    // mutate colors
     case 6:
-      mutateGene.r+=shiftAmount;
+      mutateGene.r += shiftAmount;
       break;
     case 7:
-      mutateGene.g+=shiftAmount;
+      mutateGene.g += shiftAmount;
       break;
     case 8:
-      mutateGene.b+=shiftAmount;
+      mutateGene.b += shiftAmount;
       break;
     case 9:
-      mutateGene.a+=shiftAmount;
+      mutateGene.a += shiftAmount;
       break;
-   //translate x/y coordinates
+    // translate x/y coordinates
     }
   }
   
+  public static int getAlleleValue(Gene mutateGene, int allele)
+  {
+    switch (allele)
+    {
+    case 0:
+      return mutateGene.xpoints[0];
+    case 1:
+      return mutateGene.ypoints[0];
+    case 2:
+      return mutateGene.xpoints[1];
+    case 3:
+      return mutateGene.ypoints[1];
+    case 4:
+      return mutateGene.xpoints[2];
+    case 5:
+      return mutateGene.ypoints[2];
+    case 6:
+      return mutateGene.r;
+    case 7:
+      return mutateGene.g;
+    case 8:
+      return mutateGene.b;
+    case 9:
+      return mutateGene.a;
+    }
+    return 0;
+  }
+
   private static int mutateXCoord(int coord)
   {
-    int delta =rand.nextInt(maxCoordShift);
-    delta-=(maxCoordShift/2);//delta now positve or negative
-    if(coord+delta>refGen.IMG_WIDTH||coord+delta<0)
+    int delta = rand.nextInt(maxCoordShift);
+    delta -= (maxCoordShift / 2);// delta now positve or negative
+    if (coord + delta > refGen.IMG_WIDTH || coord + delta < 0)
     {
-      //if new coord would be out of bounds, change the sign of delta
-      delta*=(-1);
-    }    
-    return (coord+=delta);
+      // if new coord would be out of bounds, change the sign of delta
+      delta *= (-1);
+    }
+    return (coord += delta);
   }
-  
+
   private static int mutateYCoord(int coord)
   {
-    int delta =rand.nextInt(maxCoordShift);
-    delta-=(maxCoordShift/2);//delta now positve or negative
-    if(coord+delta>refGen.IMG_HEIGHT||coord+delta<0)
+    int delta = rand.nextInt(maxCoordShift);
+    delta -= (maxCoordShift / 2);// delta now positve or negative
+    if (coord + delta > refGen.IMG_HEIGHT || coord + delta < 0)
     {
-      //if new coord would be out of bounds, change the sign of delta
-      delta*=(-1);
-    }    
-    return (coord+delta);
+      // if new coord would be out of bounds, change the sign of delta
+      delta *= (-1);
+    }
+    return (coord + delta);
   }
-  
+
   private static int mutateColor(int color)
   {
-    int delta=rand.nextInt(50);//50 is 1/5th 0-255
-    delta-=(50/2);//equal chance to be positive or negative
-    if(color+delta>255||color+delta<0)
+    int delta = rand.nextInt(50);// 50 is 1/5th 0-255
+    delta -= (50 / 2);// equal chance to be positive or negative
+    if (color + delta > 255 || color + delta < 0)
     {
-      delta*=(-1);
+      delta *= (-1);
     }
-    
-    return (color+delta);
+
+    return (color + delta);
   }
-  
-  private static int mutateTranslation(int coord)//move all x's or y's
+
+  private static int mutateTranslation(int coord)// move all x's or y's
   {
-    //TODO write this method
-    return coord;    
+    // TODO write this method
+    return coord;
   }
-  
+
 }
